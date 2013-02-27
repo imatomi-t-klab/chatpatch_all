08d3848447a4d6fdd6d58092a10da3e1c304aa67
imatomi-t <imatomi-t@klab.com>
2013-02-27 16:56:27 +0900

    やっぱりAPIたたいても自分のdescription取るのムリポ

--- chatwork_nanpa.js
+++ chatwork_nanpa.js
@@ -53,25 +53,47 @@ _CW.Aspect = {
 }
 _CW.ex_notice = {
     arr : [],
-    key : ['今冨','Imatomi','Tomoki','newfantasy','deploy'],
-    keyword : {},
-    notification : {},
-    exist_keyword : function (str){
-        var k = this.key;
+    hkeyword : [],
+    keyword : [],
+//    hkeyword : ['今冨','Imatomi','Tomoki','newfantasy','deploy'],
+//    keyword : ['今冨','Imatomi','Tomoki','newfantasy','deploy','重要','共有','確認','タスク'],
+    set_keyword : function (room){
+        var k;
+        if(room.type=="my" && room.description){
+            var dk = {};
+            matches = room.description.match(/\[Highlight:(.*)\]/m);
+            if(matches!==null){
+                k = this.hkeyword;
+                if(matches.length>0 && matches[1].length > 0){
+                    dk = matches[1].split(',');
+                    dk.forEach(function(v,i){
+                        if(k.indexOf(v)==-1) k.push(v);
+                    });
+                    _CW.ex_notice.hkeyword = k;
+                }
+            }
+            matches = room.description.match(/\[Keyword:(.*)\]/m);
+            if(matches!==null){
+                k = this.keyword;
+                if(matches.length>0 && matches[1].length > 0){
+                    dk = matches[1].split(',');
+                    dk.forEach(function(v,i){
+                        if(k.indexOf(v)==-1) k.push(v);
+                    });
+                    _CW.ex_notice.keyword = k;
+                }
+            }
+        }
+    },
+    exist_highlight : function (str){
+        var k = this.hkeyword;
         for(var i=0; i<k.length; i++){
             if( str.match( new RegExp( k[i], 'i' ) ) ) return true; 
         }
         return false;
     },
-    exist_notifications : function (str){
-        var k = this.key, dk;
-        var id = (location.hash.indexOf('rid') > -1)? location.hash.match(/rid([0-9]*)/)[1]: false;
-        if(id && this.notification[id]!==undefined){
-            dk = this.notification[id];
-            for(var i=0; i<dk.length; i++){
-                if( str.match( new RegExp( dk[i], 'i' ) ) ) return true; 
-            }
-        }
+    exist_keyword : function (str){
+        var k = this.keyword;
         for(var i=0; i<k.length; i++){
             if( str.match( new RegExp( k[i], 'i' ) ) ) return true; 
         }
@@ -80,7 +102,6 @@ _CW.ex_notice = {
     popup : function(id, tm, a, b ,c){
         if(window.webkitNotifications.checkPermission() == 0){
             if(this.arr.indexOf(id+'-'+tm) == -1){
-                this.arr.push(id+'-'+tm);
                 var popup = window.webkitNotifications.createNotification(a, b ,c);
                 popup.room_id = id;
                 popup.tm = tm;
@@ -93,78 +114,50 @@ _CW.ex_notice = {
             }
         }
     },
-    room : function(){
-        if(!RM.description) return;
-        var id = RM.id, des = RM.description;
-        if(this.keyword[id]===undefined){
-            this.keyword[id] = [];
-            matches = des.match(/\[Keyword:(.*)\]/m);
-            if(matches!==null){
-                if(matches.length>0 && matches[1].length > 0){
-                    this.keyword[id] = matches[1].split(',');
-                }
-            }
-        }
-        if(this.notification[id]===undefined){
-            this.notification[id] = [];
-            matches = des.match(/\[Notification:(.*)\]/m);
-            if(matches!==null){
-                if(matches.length>0 && matches[1].length > 0){
-                    this.notification[id] = matches[1].split(',');
-                }
-            }
-        }
-    },
-    room_keyword_exist : function(str){
-        if(str===undefined) return;
-        if(this.keyword[RM.id]!==undefined){
-            var dk = this.keyword[RM.id];
-            for(var i=0; i<dk.length; i++){
-                if( str.match( new RegExp( dk[i], 'i' ) ) ) return true; 
-            }
-        }
-        return false;
-    },
-    room_keyword : function(msg){
-        if(msg===undefined) return;
-        if(this.keyword[RM.id]!==undefined){
-            var dk = this.keyword[RM.id];
-            for(var i=0; i<dk.length; i++){
-                var matches = msg.match( new RegExp( dk[i], 'i' ) );
-                if(matches!=null){
-                    for(var v=0;v<matches.length;v++){
-                        if(!msg.match('<span class="chrome_extension_highlight">'+matches[v]+'</span>')){
-                            msg = msg.replaceAll( matches[v], '<span class="chrome_extension_highlight">'+matches[v]+'</span>');
-                        }
+    highlight : function(msg){
+        if(msg===undefined || !msg) return;
+        var dk = this.hkeyword
+        for(var i=0; i<dk.length; i++){
+            var matches = msg.match( new RegExp( dk[i], 'i' ) );
+            if(matches!=null){
+                for(var v=0;v<matches.length;v++){
+                    if(!msg.match('<span class="chrome_extension_highlight">'+matches[v]+'</span>')){
+                        msg = msg.replaceAll( matches[v], '<span class="chrome_extension_highlight">'+matches[v]+'</span>');
                     }
                 }
             }
-            return msg;
         }
-        return null;
+        return msg;
     }
 }
 
-$(function() {
+window.addEventListener('load',function(e){
     var aspect = function(invocation){
-        var a = invocation, ti = this;
-        var i = !1, n = !1, h = {}, j;
+        var a = invocation;
         var ch_notification = (location.hash.indexOf('rid') > -1)? location.hash.match(/rid([0-9]*)/)[1]: false;
         for (j in a) {
-            rval = a[j];
+            var rval = a[j];
             RL.rooms[j] == void 0 && (RL.rooms[j] = new Room(j));
+            if(RL.rooms[j]===undefined) continue;
             var g = RL.rooms[j],
                 k = g.getUnreadNum();
+            g.keyword_num = 0;
+            _CW.ex_notice.set_keyword(g);
             if (rval.chat_list && rval.chat_list.length > 0) {
                 for (var v = rval.chat_list.length-1, cnt=0; cnt < k; v--, cnt++){
                     if(rval.chat_list[v] === undefined) continue;
-                    if(_CW.ex_notice.exist_notifications(rval.chat_list[v].msg)){
+                    if(_CW.ex_notice.exist_keyword(rval.chat_list[v].msg)){
+                        /* popup */
                         g.keyword_num++;
                         if (window.webkitNotifications){
-                            _CW.ex_notice.popup(j,rval.chat_list[v].id,CW.getAvatarPanel(rval.chat_list[v].aid, {src: !0}),RL.rooms[j].getName(),AC.getName(rval.chat_list[v].aid) + ":" + rval.chat_list[v].msg);
+                            _CW.ex_notice.popup(j,rval.chat_list[v].id,CW.getAvatarPanel(rval.chat_list[v].aid, {src: !0}),g.getName(),AC.getName(rval.chat_list[v].aid) + ":" + rval.chat_list[v].msg);
+                        }
+                        /* room group highlight */
+                        d = $('#cw_r'+j);
+                        if(!d.hasClass('ui_chat_highlight')){
+                            d.addClass("selected");
                         }
                     }
-                    if(g.keyword_num===k) break;
                 }
             }
         }
@@ -172,35 +165,32 @@ $(function() {
     _CW.Aspect.before(RL, ["updateRoomData"], aspect);
     var aspect_hilight = function(invocation){
         if(invocation.length==0) return;
-        var glist = invocation,
-            nohighlight = (location.hash.indexOf('rid')>-1)? location.hash.match(/rid([0-9]*)/)[1]: null;
-        for(var v=0;v<glist.length;v++){
-            var room = RL.rooms[glist[v]];
+        var nohighlight = (location.hash.indexOf('rid')>-1)? location.hash.match(/rid([0-9]*)/)[1]: null;
+        invocation.forEach(function(v,i){
+            var room = RL.rooms[v];
+            _CW.ex_notice.set_keyword(room);
             if(room.timeline!==null && room.timeline.chat_list!==null){
-                var r = room.timeline.chat_list, d, unread = room.getUnreadNum();
-                /* room group highlight */
-                for(var i=room.timeline.chat_list.length-1,cnt=0 ;cnt<unread;i--,cnt++){
-                    d = $('#cw_r'+glist[v]);
-                    $.data(d,'unread',false);
-                    if(!d.hasClass('ui_chat_highlight') && _CW.ex_notice.exist_keyword(r[i].msg)){
-                        d.addClass("ui_chat_highlight");
-                        $.data(d,'unread',true);
+                var r = room.timeline.chat_list;
+                if(room.id == nohighlight){
+                    for(var i=0;i<r.length;i++){
+                        /* room timeline highlight */
+                        var target = $('#cw_c'+r[i].id+' pre');
+                        var msg = _CW.ex_notice.highlight(target.html());
+                        if( msg!=null ){
+                            target.html(msg);
+                        }
+                        
                     }
                 }
-                /* room timeline highlight */
-                if(room.id == nohighlight){
-                    _CW.ex_notice.room();
-                    for(var i=0;i<room.timeline.chat_list.length;i++){
-                        if(_CW.ex_notice.room_keyword_exist(r[i].msg)){
-                            var msg = _CW.ex_notice.room_keyword(r[i].msg);
-                            if( msg!=null ){
-                                $('#cw_c'+r[i].id+' pre').html(msg);
-                            }
-                        }
-                    }
+                if(room.type=="my"){
+                    var a = {cmd: "get_description",room_id: room.id};
+                    console.dir(a)
+                    CW.get("gateway.php", a, function(a) {
+                        console.dir(a)
+                    });
                 }
             }
-        }
+        });
         $('#cw_r'+nohighlight).removeClass('ui_chat_highlight');
         return;
     };
